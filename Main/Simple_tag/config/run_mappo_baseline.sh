fd66e7760d85e7f06ce206f05596c7196f4ed710
LarsHolwerda <lardy.holwerda@gmail.com>
2025-10-08 10:49:31 +0200

    Update run_baseline.sh
    Output to logs files and correct conda init

--- Main/Simple_tag/config/run_mappo_baseline.sh
+++ Main/Simple_tag/config/run_mappo_baseline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=mappo_baseline
-#SBATCH --output=logs/mappo_baseline_%j.out
-#SBATCH --error=logs/mappo_baseline_%j.err
+#SBATCH --output=/scratch/7990537/Hierarchical-graph-based-MARL-for-strategic-and-diverse-coordination/Main/Simple_tag/logs/mappo_baseline_%j.out
+#SBATCH --error=/scratch/7990537/Hierarchical-graph-based-MARL-for-strategic-and-diverse-coordination/Main/Simple_tag/logs/mappo_baseline_%j.err
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -16,13 +16,13 @@ echo "Starting job on $(hostname) at $(date)"
 
 cd /scratch/7990537/Hierarchical-graph-based-MARL-for-strategic-and-diverse-coordination/Main/Simple_tag
 
-/scratch/7990537/conda/simple_tag/bin/python main.py --exp-name "MAPPO" \
+/scratch/7990537/conda/envs/simple_tag/bin/python main.py --exp-name "MAPPO" \
                --algorithm "MAPPO" \
                --env-id "simple_tag" \
                --use-cuda False \
                --number-of-workers 16 \
                --seed 0 \
-                --track True \
+               --track True \
                --wandb-project-name "Simple Tag" \
                --wandb-entity "lars-holwerda-utrecht-university" \
                --env-steps-per-batch 60000 \
